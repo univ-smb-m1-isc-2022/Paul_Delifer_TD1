591f4c1208afc0d8e425d194cb66f141091a9032
Paul Delifer <paul.delifer@yahoo.fr>
2022-12-05 16:20:14 +0100

    ajout dep

--- src/main/java/PROJECT/MailValidator.java
+++ src/main/java/PROJECT/MailValidator.java
@@ -1,8 +1,14 @@
 package PROJECT;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
 public class MailValidator {
     public static boolean validate(String email) {
+
+        return EmailValidator.getInstance().isValid(email);
+
+        /*
         return email.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                "A-Z]{2,7}$");
+                "A-Z]{2,7}$");*/
     }
 }
